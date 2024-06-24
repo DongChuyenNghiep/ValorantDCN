5d4f41e9946cbea6a1db5769eff46aa5c7ab714b
DongChuyenNghiep <tuan22000@student.hamk.fi>
2024-06-24 16:29:57 +0700

    big update

--- api/index.js
+++ api/index.js
@@ -1,17 +1,6 @@
 import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js'
-dotenv.config();
-mongoose.connect(process.env.MONGO).then(() =>{
-    console.log('Connected to MongoDB');
-})
-.catch((err)=>{
-    console.log(err);
-});
 const app = express();
-app.use(express.json());
-app.listen(3000,()=>{
-  console.log('Server listening on port 3000');
-})
-app.use("/api/user",userRouter)
+
+app.listen(3000,()=> {
+    console.log('Server listening on port 3000');
+});
